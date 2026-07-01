d7b343cca69285087bdf5479dc1faa9690939e2a
Philippe Debeaumont <60342297+philippedebeaumont@users.noreply.github.com>
2026-07-01 22:22:53 +0200

    Implement Linear regression training (Math Foundations 6/6)

---
+++ foundations/linear_regression_training.py
@@ -0,0 +1,26 @@
+import numpy as np
+from numpy.typing import NDArray
+
+
+class Solution:
+    def get_derivative(self, model_prediction: NDArray[np.float64], ground_truth: NDArray[np.float64], N: int, X: NDArray[np.float64], desired_weight: int) -> float:
+        return -2 * np.dot(ground_truth - model_prediction, X[:, desired_weight]) / N
+
+    def get_model_prediction(self, X: NDArray[np.float64], weights: NDArray[np.float64]) -> NDArray[np.float64]:
+        return np.squeeze(np.matmul(X, weights))
+
+    learning_rate = 0.01
+
+    def train_model(
+        self,
+        X: NDArray[np.float64],
+        Y: NDArray[np.float64],
+        num_iterations: int,
+        initial_weights: NDArray[np.float64]
+    ) -> NDArray[np.float64]:
+        weights = initial_weights
+        for _ in range(num_iterations):
+            preds = self.get_model_prediction(X, weights)
+            for j in range(len(weights)):
+                weights[j] -= self.learning_rate * self.get_derivative(preds, Y, len(X), X, j)
+        return np.round(weights, 5)
